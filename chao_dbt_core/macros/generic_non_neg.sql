df7f86857919a8c3b624886518bc62085b2c14d2
xc797 <chao.xu268@gmail.com>
2026-06-09 16:27:47 +1000

    add comment in generic_non_neg doc

--- chao_dbt_core/macros/generic_non_neg.sql
+++ chao_dbt_core/macros/generic_non_neg.sql
@@ -1,7 +1,6 @@
+-- purpose: generic test to check if a column has negative values
 {% test generic_non_neg(model, column_name) %}
 
-    select *
-    from {{ model }}
-    where {{ column_name }} < 0
+    select * from {{ model }} where {{ column_name }} < 0
 
 {% endtest %}
